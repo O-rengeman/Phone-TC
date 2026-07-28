9d9e12eebd3d76cd0974ab563abfa29d8571187e
man orenge <kamonohasidazo@gmail.com>
2026-07-29 01:08:27 +0900

    feat: enable Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.tsx'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
+    <SpeedInsights />
   </StrictMode>,
 )
